ef0e9ad11ea0234628d19de38272247399368eb1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-03-06 08:47:26 -0500

    Add the YearChecker class with the correct TODO markers.

---
+++ src/main/java/practicalseven/YearChecker.java
@@ -0,0 +1,60 @@
+package practicalseven;
+
+public class YearChecker {
+
+  /**
+   * The year to be checked for the YearChecker class.
+   **/
+  int year;
+
+  // TODO: Use the REFERENCE_YEAR variable in the
+  // implementation of certain methods in this class
+  // Please see the assignment sheet for additional details
+
+  /**
+   * The reference year for the checking.
+   **/
+  private static final int REFERENCE_YEAR = 2013;
+
+  /**
+   * The constructor for the YearChecker.
+   **/
+  public YearChecker(int startingYear) {
+    year = startingYear;
+  }
+
+  // TODO: Add an implementation of each of these methods
+  // TODO: Please refer to the assignment sheet for more
+  // details about the conditional logic that you must add
+
+  /**
+   * Determine if the year is a leap year.
+   *
+   * @return A boolean giving an answer to the question.
+   **/
+  public boolean isLeapYear() {
+    boolean isLeapYear = false;
+    return isLeapYear;
+  }
+
+  /**
+   * Determine if the year is a cicada year.
+   *
+   * @return A boolean giving an answer to the question.
+   **/
+  public boolean isCicadaYear() {
+    boolean isCicadaYear = false;
+    return isCicadaYear;
+  }
+
+  /**
+   * Determine if the year is a sunspot year.
+   *
+   * @return A boolean giving an answer to the question.
+   **/
+  public boolean isSunspotYear() {
+    boolean isSunspotYear = false;
+    return isSunspotYear;
+  }
+
+}
